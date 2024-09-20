8fdc47f66417a6731829d99a9bca914d79eb59cc
vincnetwu911-euh <vincentc.wu@gmail.com>
2024-09-19 20:07:14 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "vincentwu1107-org>"
+    organization = "NYULANGONE"
     workspaces {
       name = "ace-iac-day-zero"
     }
